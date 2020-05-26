ddfb6016de6b506898ce85cffaab37c79bf4ac32
Laurence Jones <laurence.jones@infinityworks.com>
2020-05-26 13:02:27 +0100

    Retry adding IP to WAF if lock token has been used

--- ci_scripts/cleanup_waf_ipset.sh
+++ ci_scripts/cleanup_waf_ipset.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # This script will remove all IPs from the CI WAF IP Set in order to prevent it being cluttered with IPs from previous builds
 
 LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
--- ci_scripts/launch_browserstack_test.sh
+++ ci_scripts/launch_browserstack_test.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
 # This script will update the WAF with the IP of the CI box before installing the BrowserStack Local CLI
 # to setup a tunnel and then run the tests on the required browser
 
 BROWSER=$1
 
+update_ip_set() {
+    IP=$(wget -qO- http://checkip.amazonaws.com)
+    LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
+    NEXT_LOCK_TOKEN=$(aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses $IP/32 --lock-token $LOCK_TOKEN --region $AWS_REGION 2>/dev/null | jq .NextLockToken | tr -d '"')
+
+    if [ -z "$NEXT_LOCK_TOKEN" ]; then
+        return 1
+    fi
+}
+
 sudo apt update
 sudo apt install jq unzip
 
-IP=$(wget -qO- http://checkip.amazonaws.com)
-LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
+n=0
 
-NEXT_LOCK_TOKEN=$(aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses $IP/32 --lock-token $LOCK_TOKEN --region $AWS_REGION | jq .NextLockToken | tr -d '"')
+until [ "$n" -ge 5 ]; do
+    update_ip_set && break
+    n=$((n+1))
+    sleep 5
+done
 
 wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
 unzip BrowserStackLocal-linux-x64.zip
