f144e751090337e792a58fee07914e4e573c428f
Laurence Jones <laurence.jones@infinityworks.com>
2020-05-26 15:49:26 +0100

    Run all ui tests as single script

--- ci_scripts/cleanup_waf_ipset.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# This script will remove all IPs from the CI WAF IP Set in order to prevent it being cluttered with IPs from previous builds
-
-LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
-
-aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses --lock-token $LOCK_TOKEN --region $AWS_REGION
