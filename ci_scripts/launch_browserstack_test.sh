3e43d8e18f1a4efa4eb5868e74afa17d6b9d12b2
Laurence Jones <laurence.jones@infinityworks.com>
2020-05-26 14:56:40 +0100

    Run all ui tests from single script

--- ci_scripts/launch_browserstack_test.sh
+++ ci_scripts/launch_browserstack_test.sh
@@ -5,8 +5,6 @@ set -e
 # This script will update the WAF with the IP of the CI box before installing the BrowserStack Local CLI
 # to setup a tunnel and then run the tests on the required browser
 
-BROWSER=$1
-
 update_ip_set() {
     IP=$(wget -qO- http://checkip.amazonaws.com)
     LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
@@ -19,12 +17,13 @@ update_ip_set() {
 }
 
 run_ui_tests() {
-    wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
-    unzip BrowserStackLocal-linux-x64.zip
+    mvn -Dhost=remote -Dbrowser=$1
+}
 
-    ./BrowserStackLocal --key $BROWSERSTACK_KEY --daemon start --force-local
+cleanup_ip_set() {
+    LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
 
-    mvn -Dhost=remote -Dbrowser=$BROWSER test
+    aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses --lock-token $LOCK_TOKEN --region $AWS_REGION
 }
 
 sudo apt update
@@ -33,7 +32,16 @@ sudo apt install jq unzip
 n=0
 
 until [ "$n" -ge 5 ]; do
-    update_ip_set && break && run_ui_tests
+    update_ip_set && break
     n=$((n+1))
     sleep 5
 done
+
+wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+unzip BrowserStackLocal-linux-x64.zip
+
+./BrowserStackLocal --key $BROWSERSTACK_KEY --daemon start --force-local
+
+run_ui_tests chrome & run_ui_tests firefox & run_ui_tests ie
+
+cleanup_ip_set
