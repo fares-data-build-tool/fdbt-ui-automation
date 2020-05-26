d45770edb2bdf16dda5517084b871e85b207064f
Laurence Jones <laurence.jones@infinityworks.com>
2020-05-26 14:44:21 +0100

    CI scripts tweak

--- ci_scripts/launch_browserstack_test.sh
+++ ci_scripts/launch_browserstack_test.sh
@@ -13,8 +13,18 @@ update_ip_set() {
     NEXT_LOCK_TOKEN=$(aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses $IP/32 --lock-token $LOCK_TOKEN --region $AWS_REGION 2>/dev/null | jq .NextLockToken | tr -d '"')
 
     if [ -z "$NEXT_LOCK_TOKEN" ]; then
+        echo "RETRYING"
         return 1
     fi
+}
+
+run_ui_tests() {
+    wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+    unzip BrowserStackLocal-linux-x64.zip
+
+    ./BrowserStackLocal --key $BROWSERSTACK_KEY --daemon start --force-local
+
+    mvn -Dhost=remote -Dbrowser=$BROWSER test
 }
 
 sudo apt update
@@ -23,14 +33,7 @@ sudo apt install jq unzip
 n=0
 
 until [ "$n" -ge 5 ]; do
-    update_ip_set && break
+    update_ip_set && break && run_ui_tests
     n=$((n+1))
     sleep 5
 done
-
-wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
-unzip BrowserStackLocal-linux-x64.zip
-
-./BrowserStackLocal --key $BROWSERSTACK_KEY --daemon start --force-local
-
-mvn -Dhost=remote -Dbrowser=$BROWSER test
