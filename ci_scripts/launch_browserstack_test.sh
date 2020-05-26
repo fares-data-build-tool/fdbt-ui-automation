16b3c87ba7a3046106b9bee02df718724dbe77ac
Laurence Jones <laurence.jones@infinityworks.com>
2020-05-26 15:35:49 +0100

    Remove echo

--- ci_scripts/launch_browserstack_test.sh
+++ ci_scripts/launch_browserstack_test.sh
@@ -11,7 +11,6 @@ update_ip_set() {
     NEXT_LOCK_TOKEN=$(aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses $IP/32 --lock-token $LOCK_TOKEN --region $AWS_REGION 2>/dev/null | jq .NextLockToken | tr -d '"')
 
     if [ -z "$NEXT_LOCK_TOKEN" ]; then
-        echo "RETRYING"
         return 1
     fi
 }
@@ -22,7 +21,6 @@ run_ui_tests() {
 
 cleanup_ip_set() {
     LOCK_TOKEN=$(aws wafv2 get-ip-set --scope REGIONAL --region $AWS_REGION --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
-
     aws wafv2 update-ip-set --scope REGIONAL --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses --lock-token $LOCK_TOKEN --region $AWS_REGION
 }
 
