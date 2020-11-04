90f96a522d9f9901bed0f9de21839bb9a0b40297
Rob Catton <robmcatton@gmail.com>
2020-11-04 11:53:18 +0000

    Changed 2 tests to also select a duration type

--- src/test/java/uk/co/tfn/UserJourneyTests.java
+++ src/test/java/uk/co/tfn/UserJourneyTests.java
@@ -159,6 +159,7 @@ public class UserJourneyTests {
         helpers.sendKeysById("product-details-price", "10.50");
         helpers.continueButtonClick();
         helpers.sendKeysById("validity", "1");
+        helpers.selectRandomOptionFromDropdownById("validity-units");
         helpers.continueButtonClick();
 
         String endOfCalendarOption = "period-end-calendar";
@@ -194,6 +195,7 @@ public class UserJourneyTests {
         helpers.sendKeysById("product-details-price", "10.50");
         helpers.continueButtonClick();
         helpers.sendKeysById("validity", "1");
+        helpers.selectRandomOptionFromDropdownById("validity-units");
         helpers.continueButtonClick();
 
         String endOfCalendarOption = "period-end-calendar";
